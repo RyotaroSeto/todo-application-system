26a4c545ed8e75309d5dfbfd46ad4df51587f848
RyotaroSeto <hahahahawaiwai321@gmail.com>
2023-12-03 18:58:57 +0900

    Refactor main function and add server setup

--- auth_service/main.go
+++ auth_service/main.go
@@ -1,7 +1,42 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"log"
+	"net"
+	"os/signal"
+	"syscall"
+
+	"auth_service/infra/config"
+)
 
 func main() {
-	fmt.Println("Hello, auth!")
+	ctx, cancel := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	defer cancel()
+
+	repo, err := config.Load(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	l, err := net.Listen("tcp", fmt.Sprintf(":%s", repo.Port))
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(l)
+
+	// svr := setupServer(repo)
+	// go func() {
+	// 	<-ctx.Done()
+	// 	svr.GracefulStop()
+	// }()
+
+	// if err := svr.Serve(l); err != nil {
+	// 	log.Fatal(err)
+	// }
 }
+
+// func setupServer(repo *config.Config) interface{} {
+// 	return nil
+// }

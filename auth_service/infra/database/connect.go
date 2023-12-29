a29ff18f05b5f64afc85680fd3b8cd9b92bacd6f
RyotaroSeto <hahahahawaiwai321@gmail.com>
2023-12-29 19:39:03 +0900

    Add error handling and connection pool configuration to database Connect function

--- auth_service/infra/database/connect.go
+++ auth_service/infra/database/connect.go
@@ -1,6 +1,8 @@
 package database
 
 import (
+	"pkg/errors"
+
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
@@ -11,16 +13,32 @@ type DB struct {
 
 var d *DB
 
-func Connect(dsn string) error {
+func Connect(dsn string, maxConn, maxIdle int) error {
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		return err
+	}
+
+	sqlDB, err := db.DB()
+	if err != nil {
+		return errors.Errorf(errors.InternalServerError, "failed to get sql.DB: %w", err)
+	}
+	if err = sqlDB.Ping(); err == nil {
+		return errors.Errorf(errors.InternalServerError, "failed to ping: %w", err)
+	}
+
+	if maxConn > 0 {
+		sqlDB.SetMaxOpenConns(maxConn)
+	}
+	if maxIdle > 0 {
+		sqlDB.SetMaxIdleConns(maxIdle)
 	}
 
 	d = &DB{db}
 
 	return nil
 }
+
 func Get() *DB {
 	return d
 }

f37dfa3b2338c21c2ce6e75fc6f5d8045ba99d7d
RyotaroSeto <hahahahawaiwai321@gmail.com>
2023-12-29 15:51:30 +0900

    Refactor gateway handler and update dependencies

--- gateway/handlers/handler.go
+++ gateway/handlers/handler.go
@@ -2,29 +2,57 @@ package handlers
 
 import (
 	"context"
-	"fmt"
-	"gateway/configs"
+	"gen/go/todo"
 	"net/http"
+	"net/http/httputil"
+	"net/url"
+	"pkg/errors"
+
+	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 )
 
-func NewHandler(ctx context.Context, cfg *configs.Config) (http.Handler, error) {
+const (
+	grpcServerAddress = "todo_service:8080"
+	docsServerAddress = "http://docs-server:8080"
+)
+
+func NewHandler(ctx context.Context) (http.Handler, error) {
+	grpcGateway := runtime.NewServeMux()
+	opts := []grpc.DialOption{
+		grpc.WithTransportCredentials(insecure.NewCredentials()),
+	}
+
+	if err := todo.RegisterTodoApiHandlerFromEndpoint(ctx, grpcGateway, grpcServerAddress, opts); err != nil {
+		return nil, err
+	}
+
+	docsURL, err := url.Parse(docsServerAddress)
+	if err != nil {
+		return nil, errors.Newf(errors.InternalServerError, "failed to parse docsServerAddress=%v:%s", docsServerAddress, err)
+	}
+	docsProxy := httputil.NewSingleHostReverseProxy(docsURL)
+
 	mux := http.NewServeMux()
 
-	mux.HandleFunc("/todo", corsMiddleware(todoHandler))
+	// mux.HandleFunc("/todo", corsMiddleware(todoHandler))
+	mux.Handle("/docs/", docsProxy)
+	mux.Handle("/", grpcGateway)
 	return mux, nil
 }
 
-func todoHandler(w http.ResponseWriter, r *http.Request) {
-	switch r.Method {
-	case http.MethodGet:
-		fmt.Fprint(w, "GET hello!\n")
-	case http.MethodPost:
-		fmt.Fprint(w, "POST hello!\n")
-	case http.MethodPut:
-		fmt.Fprint(w, "PUT hello!\n")
-	case http.MethodDelete:
-		fmt.Fprint(w, "DELETE hello!\n")
-	default:
-		fmt.Fprint(w, "Method not allowed.\n")
-	}
-}
+// func todoHandler(w http.ResponseWriter, r *http.Request) {
+// 	switch r.Method {
+// 	case http.MethodGet:
+// 		fmt.Fprint(w, "GET hello!\n")
+// 	case http.MethodPost:
+// 		fmt.Fprint(w, "POST hello!\n")
+// 	case http.MethodPut:
+// 		fmt.Fprint(w, "PUT hello!\n")
+// 	case http.MethodDelete:
+// 		fmt.Fprint(w, "DELETE hello!\n")
+// 	default:
+// 		fmt.Fprint(w, "Method not allowed.\n")
+// 	}
+// }

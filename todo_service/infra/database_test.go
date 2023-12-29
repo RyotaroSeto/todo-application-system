6a984b41c133e7dd6dc2f923353fb53987533239
RyotaroSeto <hahahahawaiwai321@gmail.com>
2023-12-29 23:27:23 +0900

    Refactor database_test.go by removing unused TestDBConnect function

--- todo_service/infra/database_test.go
+++ todo_service/infra/database_test.go
@@ -6,32 +6,6 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestDBConnect(t *testing.T) {
-	type args struct {
-		dsn     string
-		maxConn int
-		maxIdle int
-	}
-	tests := []struct {
-		name      string
-		args      args
-		assertion assert.ErrorAssertionFunc
-	}{
-		{
-			name: "success",
-			args: args{
-				dsn: "host=host user=user password=password dbname=name port=5432 sslmode=disable TimeZone=Asia/Tokyo",
-			},
-			assertion: assert.NoError,
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			tt.assertion(t, DBConnect(tt.args.dsn, tt.args.maxConn, tt.args.maxIdle))
-		})
-	}
-}
-
 func TestGet(t *testing.T) {
 	tests := []struct {
 		name string
